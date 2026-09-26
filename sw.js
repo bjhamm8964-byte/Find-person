d93dda01fea16c77146e3a890139f9fb8c7547fc
bjhamm8964-byte <bjhamm8964@gmail.com>
2026-09-26 21:18:55 +0800

    Update files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "find-person-shell-v25";
-const SHELL = ["./", "index.html", "styles.css?v=20260922-5", "app.js?v=20260922-4", "manifest.webmanifest", "icon.svg"];
+const CACHE = "find-person-shell-v26";
+const SHELL = ["./", "index.html", "styles.css?v=20260926-1", "app.js?v=20260926-1", "manifest.webmanifest", "icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => {
